b4f6610c04025fbb6f15f1086efa0c2bad9ddbaa
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2021-10-05 08:25:30 +0200

    Inscription

--- view/traitement_reservation.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'model_reservation.php';
-require_once 'manager_reservation.php';
-
-$reservation = new reservation($_POST["nom"], $_POST["prenom"], $_POST["mail"], $_POST["mdp"]);
-$co = new Manager();
-$co->reservation($reservation);
-?>
