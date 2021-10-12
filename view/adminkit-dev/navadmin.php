9e0d58a0a7069fa19ba2c52c03bc29531a530d8d
bambaibrahim02 <ibrayoman02@gmail.com>
2021-10-12 11:15:23 +0200

    Datatable à jour

---
+++ view/adminkit-dev/navadmin.php
@@ -0,0 +1,77 @@
+<div class="wrapper">
+    <nav id="sidebar" class="sidebar js-sidebar">
+        <div class="sidebar-content js-simplebar">
+            <a class="sidebar-brand" href="index.html">
+                <span class="align-middle">AdminKit</span>
+            </a>
+
+            <ul class="sidebar-nav">
+                <li class="sidebar-header">
+                    Pages
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-profile.html">
+                        <i class="align-middle" data-feather="user"></i> <span class="align-middle">Consulter dossier d'admission</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-sign-in.html">
+                        <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Ajouter un Utilisateur</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-sign-up.html">
+                        <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Ajouter un administrateur</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="pages-blank.html">
+                        <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Ajouter un Medecin</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                <a class="sidebar-link" href="pages-blank.html">
+                    <i class="align-middle" data-feather="user-plus"></i> <span class="align-middle">Ajouter un Urgentiste</span>
+                </a>
+                </li>
+
+                <li class="sidebar-header">
+                    Utilisateur
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="datatableAdmin.php">
+                        <i class="align-middle" data-feather="coffee"></i> <span class="align-middle">Administrateur</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="datatableUser.php">
+                        <i class="align-middle" data-feather="square"></i> <span class="align-middle">Patient</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="datatableMedecin.php">
+                        <i class="align-middle" data-feather="check-square"></i> <span class="align-middle">Medecin</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="datatableUrgentiste.php">
+                        <i class="align-middle" data-feather="grid"></i> <span class="align-middle">Urgentiste</span>
+                    </a>
+                </li>
+
+                <li class="sidebar-item">
+                    <a class="sidebar-link" href="datatableRDV.php">
+                        <i class="align-middle" data-feather="align-left"></i> <span class="align-middle">Rendez-vous</span>
+                    </a>
+                </li>
+        </div>
+    </nav>
