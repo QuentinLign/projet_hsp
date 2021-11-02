e8701e7889bb520a42a5cbd4af6db661714dd3ff
imen23 <90242060+Azerty233@users.noreply.github.com>
2021-11-02 10:18:19 +0100

    creation,modif,admission

--- view/adminkit-dev/class/mvc/Manager_User.php
+++ view/adminkit-dev/class/mvc/Manager_User.php
@@ -139,6 +139,47 @@ class Manager_User
 
 
   }
+  public function modif_da(User $modif)
+  {
+    $bdd = new PDO('mysql:host=localhost;dbname=projet_hsp','root','');
+    $req = $bdd->prepare('UPDATE utilisateurs SET id_utilisateurs = ?, mutuelle = ?, date_naissance = ?,adresse_postale = ?,numero_secu = ?,option = ?, regime_specifique = ?, id_medecins = ?, WHERE email = ?');
+    $req->execute(array($modif->getid_utilisateurs(), $modif->getmutuelle(),$modif->getdate_naissance(),$modif->getadresse_postale(),$modif->getnumero_secu(),$modif->getoption(),$modif->getregime_specifique(),$modif->getid_medecins(), $email));
+    $_SESSION['succes_modif'] = 'Modification enregistré';
+    header('location: ../../mon-compte.php');
+    //actualisation du nom de l'utilisateur dans les pages
+    $req = $bdd->prepare('SELECT * from utilisateurs where email = ?');
+    $req->execute(array($email));
+    $donnee = $req->fetch();
+    $_SESSION['id_utilisateurs'] = $donnee['id_utilisateurs'];
+    $_SESSION['mutuelle'] = $donnee['mutuelle'];
+    $_SESSION['date_naissance'] = $donnee['date_naissance'];
+    $_SESSION['adresse_postale'] = $donnee['adresse_postale'];
+    $_SESSION['numero_secu'] = $donnee['numero_secu'];
+    $_SESSION['option'] = $donnee['option'];
+    $_SESSION['regime_specifique'] = $donnee['regime_specifique'];
+    $_SESSION['id_medecins'] = $donnee['id_medecins'];
+  }
+
+  //Update des données utilisateur dans la bdd
+  public function modif_mdp(User $verif, $mdp, $email)
+  {
+    $bdd = new PDO('mysql:host=localhost;dbname=projet_hsp','root','');
+    $req = $bdd->prepare('SELECT * from utilisateurs where email = ? AND mdp = ?');
+    $req->execute(array($email, SHA1($verif->getMdp())));
+    $donnee = $req->fetch();
+    if ($donnee)
+    {
+      $req = $bdd->prepare('UPDATE utilisateurs SET mdp = ? WHERE email = ?');
+      $req->execute(array(SHA1($mdp), $email));
+      header('location: ../../mon-compte.php');
+      $_SESSION['message_mdp'] = 'Modification enregistré';
+      echo '<scrip>$(document).ready(function()){
+        $("nav").toggleClass("active");
+        return false;
+      });</script>';
+    }
+
+
 }
   //Update des données utilisateur dans la bdd
   public function recup_mdp(User $change, $email)
