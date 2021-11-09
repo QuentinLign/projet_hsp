1ccb05fa19c1a1e07cb103e718c6e5cf3d9aa82b
bambaibrahim02 <ibrayoman02@gmail.com>
2021-11-09 09:40:08 +0100

    Mise à jour bdd

--- bdd/projet_hsp.sql
+++ bdd/projet_hsp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 09 nov. 2021 à 08:32
+-- Généré le :  mar. 09 nov. 2021 à 08:39
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -21,6 +21,75 @@ SET time_zone = "+00:00";
 --
 -- Base de données :  `projet_hsp`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `diagnostic`
+--
+
+DROP TABLE IF EXISTS `diagnostic`;
+CREATE TABLE IF NOT EXISTS `diagnostic` (
+  `id` int(10) NOT NULL,
+  `id_utilisateurs` int(10) NOT NULL,
+  `symptomes` longtext COLLATE utf8_bin NOT NULL,
+  `date` date NOT NULL,
+  `niveau_urgence` varchar(10) COLLATE utf8_bin NOT NULL,
+  `id_medec` varchar(10) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_ut_diagnostic` (`id_utilisateurs`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `dossier_admission`
+--
+
+DROP TABLE IF EXISTS `dossier_admission`;
+CREATE TABLE IF NOT EXISTS `dossier_admission` (
+  `id` int(10) NOT NULL,
+  `id_utilisateurs` int(10) NOT NULL,
+  `date_naissance` date NOT NULL,
+  `adresse_postale` varchar(40) COLLATE utf8_bin NOT NULL,
+  `mutuelle` varchar(40) COLLATE utf8_bin NOT NULL,
+  `numero_secu` int(40) NOT NULL,
+  `option` varchar(40) COLLATE utf8_bin NOT NULL,
+  `regime_specifique` varchar(40) COLLATE utf8_bin NOT NULL,
+  `id_medecins` varchar(10) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_utilisateurs` (`id_utilisateurs`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `rendezvous`
+--
+
+DROP TABLE IF EXISTS `rendezvous`;
+CREATE TABLE IF NOT EXISTS `rendezvous` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_utilisateurs` int(11) NOT NULL,
+  `nom_utilisateur` varchar(40) COLLATE utf8_bin NOT NULL,
+  `date` varchar(40) COLLATE utf8_bin NOT NULL,
+  `heure` varchar(40) COLLATE utf8_bin NOT NULL,
+  `salle` varchar(40) COLLATE utf8_bin NOT NULL,
+  `id_medecins` int(11) NOT NULL,
+  `nom_medecin` varchar(40) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `nom_utilisateur` (`nom_utilisateur`),
+  UNIQUE KEY `nom_medecin` (`nom_medecin`),
+  KEY `id_utili` (`id_utilisateurs`),
+  KEY `id_med` (`id_medecins`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Déchargement des données de la table `rendezvous`
+--
+
+INSERT INTO `rendezvous` (`id`, `id_utilisateurs`, `nom_utilisateur`, `date`, `heure`, `salle`, `id_medecins`, `nom_medecin`) VALUES
+(1, 26, '', '02/11/2002', '09:00', 'salle_1', 34, '');
 
 -- --------------------------------------------------------
 
@@ -59,6 +128,17 @@ INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `mdp`, `specialite`,
 (32, 'er', 'er', 'er@gmail.com', '602c57ffb51af99d6f3b54c0ee9587bb110fb990', NULL, NULL, NULL, 1),
 (34, 'tr', 'tr', 'tr@gmail.com', 'd9e83874d260f2f10d48d98c0b773b836096d426', NULL, NULL, 'MEDECIN', 1),
 (35, 'ty', 'ty', 'ty@gmail.com', '32ad247f77b8a066ef05467ce49a5a63e193c3a3', NULL, NULL, NULL, 1);
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `rendezvous`
+--
+ALTER TABLE `rendezvous`
+  ADD CONSTRAINT `id_med` FOREIGN KEY (`id_medecins`) REFERENCES `utilisateurs` (`id`),
+  ADD CONSTRAINT `id_utili` FOREIGN KEY (`id_utilisateurs`) REFERENCES `utilisateurs` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
