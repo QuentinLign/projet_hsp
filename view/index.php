666a483272ca73fae1cc9d98f05bfa18b7319ade
bambaibrahim02 <ibrayoman02@gmail.com>
2021-11-09 11:39:55 +0100

    ROBERT DEBRéEE

--- view/index.php
+++ view/index.php
@@ -702,7 +702,7 @@ session_start();
       </div>
 
       <div>
-        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
+        <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/place/H%C3%B4pital+Robert+Debr%C3%A9/@48.8797028,2.4000975,17z/data=!4m9!1m2!2m1!1shopital+robert+debr%C3%A9!3m5!1s0x47e66dbc97d5a691:0xf7d562c65bddd225!8m2!3d48.8794483!4d2.4013382!15sChVob3BpdGFsIHJvYmVydCBkZWJyw6mSAQl0cmFtX3N0b3A" frameborder="0" allowfullscreen></iframe>
       </div>
 
       <div class="container">
