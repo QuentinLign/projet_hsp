bc9671185cc59bf316ab39c145b85b2fce3b389f
bambaibrahim02 <ibrayoman02@gmail.com>
2021-12-07 09:09:24 +0100

    replacer la bdd

--- adminkit-dev/bdd/projet_hsp.sql
+++ adminkit-dev/bdd/projet_hsp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 05 oct. 2021 à 09:42
+-- Généré le :  mar. 07 déc. 2021 à 08:08
 -- Version du serveur :  5.7.26
 -- Version de PHP :  7.2.18
 
@@ -25,31 +25,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `administrateur`
---
-
-DROP TABLE IF EXISTS `administrateur`;
-CREATE TABLE IF NOT EXISTS `administrateur` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(40) NOT NULL,
-  `prenom` varchar(40) NOT NULL,
-  `mail` varchar(30) NOT NULL,
-  `mdp` varchar(30) NOT NULL,
-  `etat_compte` int(1) NOT NULL,
-  `derniere_connexion` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Déchargement des données de la table `administrateur`
---
-
-INSERT INTO `administrateur` (`id`, `nom`, `prenom`, `mail`, `mdp`, `etat_compte`, `derniere_connexion`) VALUES
-(1, 'LIGNANI', 'Quentin', 'qlignani@gmail.com', '1234', 1, '2021-09-21');
-
--- --------------------------------------------------------
-
---
 -- Structure de la table `diagnostic`
 --
 
@@ -64,6 +39,38 @@ CREATE TABLE IF NOT EXISTS `diagnostic` (
   PRIMARY KEY (`id`),
   KEY `id_ut_diagnostic` (`id_utilisateurs`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `doctorspecilization`
+--
+
+DROP TABLE IF EXISTS `doctorspecilization`;
+CREATE TABLE IF NOT EXISTS `doctorspecilization` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `specilization` varchar(255) DEFAULT NULL,
+  `creationDate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updationDate` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `doctorspecilization`
+--
+
+INSERT INTO `doctorspecilization` (`id`, `specilization`, `creationDate`, `updationDate`) VALUES
+(1, 'Gynecologist/Obstetrician', '2016-12-28 06:37:25', '0000-00-00 00:00:00'),
+(2, 'General Physician', '2016-12-28 06:38:12', '0000-00-00 00:00:00'),
+(3, 'Dermatologist', '2016-12-28 06:38:48', '0000-00-00 00:00:00'),
+(4, 'Homeopath', '2016-12-28 06:39:26', '0000-00-00 00:00:00'),
+(5, 'Ayurveda', '2016-12-28 06:39:51', '0000-00-00 00:00:00'),
+(6, 'Dentist', '2016-12-28 06:40:08', '0000-00-00 00:00:00'),
+(7, 'Ear-Nose-Throat (Ent) Specialist', '2016-12-28 06:41:18', '0000-00-00 00:00:00'),
+(9, 'Demo test', '2016-12-28 07:37:39', '0000-00-00 00:00:00'),
+(10, 'Bones Specialist demo', '2017-01-07 08:07:53', '0000-00-00 00:00:00'),
+(11, 'Test', '2019-06-23 17:51:06', '2019-06-23 17:55:06'),
+(12, 'Dermatologist', '2019-11-10 18:36:36', '2019-11-10 18:36:50');
 
 -- --------------------------------------------------------
 
@@ -89,66 +96,35 @@ CREATE TABLE IF NOT EXISTS `dossier_admission` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `view_medecin`
+-- Structure de la table `rendezvous`
 --
 
-DROP TABLE IF EXISTS `medecin`;
-CREATE TABLE IF NOT EXISTS `medecin` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(40) NOT NULL,
-  `prenom` varchar(40) NOT NULL,
-  `mail` varchar(30) NOT NULL,
-  `mdp` varchar(30) NOT NULL,
-  `specialite` varchar(30) NOT NULL,
-  `etat_compte` int(1) NOT NULL,
-  `derniere_connexion` date NOT NULL,
+DROP TABLE IF EXISTS `rendezvous`;
+CREATE TABLE IF NOT EXISTS `rendezvous` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` text COLLATE utf8_bin NOT NULL,
+  `prenom` text COLLATE utf8_bin NOT NULL,
+  `doctorSpecilization` varchar(255) COLLATE utf8_bin NOT NULL,
+  `doctor` varchar(255) COLLATE utf8_bin NOT NULL,
+  `RDVdate` varchar(255) COLLATE utf8_bin NOT NULL,
+  `RDVheure` varchar(255) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
--- Déchargement des données de la table `view_medecin`
+-- Déchargement des données de la table `rendezvous`
 --
 
-INSERT INTO `medecin` (`id`, `nom`, `prenom`, `mail`, `mdp`, `specialite`, `etat_compte`, `derniere_connexion`) VALUES
-(1, 'LIGNANI', 'Quentin', 'q.lignani@lprs.fr', '1234', 'Medecin generaliste', 1, '2021-09-21');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `rendez-vous`
---
-
-DROP TABLE IF EXISTS `rendez-vous`;
-CREATE TABLE IF NOT EXISTS `rendez-vous` (
-  `id` int(10) NOT NULL,
-  `id_utilisateurs` int(10) NOT NULL,
-  `date` int(10) NOT NULL,
-  `heure` int(10) NOT NULL,
-  `salle` varchar(10) COLLATE utf8_bin NOT NULL,
-  `id_medecins` int(10) NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `id_ut` (`id_utilisateurs`),
-  KEY `id_medecin` (`id_medecins`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `urgentiste`
---
-
-DROP TABLE IF EXISTS `urgentiste`;
-CREATE TABLE IF NOT EXISTS `urgentiste` (
-  `id` int(11) NOT NULL,
-  `nom` varchar(40) NOT NULL,
-  `prenom` varchar(40) NOT NULL,
-  `mail` varchar(30) NOT NULL,
-  `mdp` varchar(30) NOT NULL,
-  `specialite` varchar(30) NOT NULL,
-  `etat_compte` int(1) NOT NULL,
-  `derniere_connexion` date NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+INSERT INTO `rendezvous` (`id`, `nom`, `prenom`, `doctorSpecilization`, `doctor`, `RDVdate`, `RDVheure`) VALUES
+(1, 'jam', 'jam', 'Gynecologist/Obstetrician', 'ty', '2021-12-02', '19:16'),
+(2, 'yo', 'man', 'Homeopath', 'ty', '2021-12-01', '21:03'),
+(3, 'yo', 'man', 'Homeopath', 'ty', '2021-12-01', '21:03'),
+(4, 'tr', 'tr', 'Ayurveda', 'ty', '2021-12-01', '21:08'),
+(5, 'tr', 'tan', 'Homeopath', 'az', '2021-12-05', '13:11'),
+(6, 'za', 'tan', 'Ayurveda', 'az', '2021-12-05', '13:14'),
+(7, 'thanos', 'op', 'Ayurveda', 'az', '', ''),
+(8, 'thanos', 'er', 'Dermatologist', 'az', '2021-12-05', '13:36'),
+(9, 'tr', 'er', 'Dermatologist', 'az', '2021-12-05', '16:53');
 
 -- --------------------------------------------------------
 
@@ -163,29 +139,20 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `prenom` varchar(40) COLLATE utf8_bin NOT NULL,
   `email` varchar(40) COLLATE utf8_bin NOT NULL,
   `mdp` varchar(40) COLLATE utf8_bin NOT NULL,
+  `specialite` varchar(20) COLLATE utf8_bin DEFAULT NULL,
+  `date_connexion` date DEFAULT NULL,
+  `role` varchar(10) COLLATE utf8_bin DEFAULT NULL,
   `verif` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=57 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `mdp`, `verif`) VALUES
-(13, 'qq', 'qffff', 'qlignani@gmail.com', '22ea1c649c82946aa6e479e1ffd321e4a318b1b0', 0),
-(14, 'quentin', 'lignnai', 'q@qq.com', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8', 0),
-(15, 'lig', 'q', 'q.q@gmail.com', '86f7e437faa5a7fce15d1ddcb9eaeaea377667b8', 0),
-(18, 'qq', 'q', 'q.lignani@lprs.fr', '8471a3b410c2a100d5b6285018955718f1c26368', 1);
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `rendez-vous`
---
-ALTER TABLE `rendez-vous`
-  ADD CONSTRAINT `id_medecin` FOREIGN KEY (`id_medecins`) REFERENCES `medecin` (`id`);
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `email`, `mdp`, `specialite`, `date_connexion`, `role`, `verif`) VALUES
+(35, 'ty', 'ty', 'ty@gmail.com', '32ad247f77b8a066ef05467ce49a5a63e193c3a3', NULL, '2021-12-06', 'ADMIN', 1),
+(53, 'az', 'az', 'q.lignani@lprs.fr', '90283840d90de49b8e7984bd99b47fee0d4bd50d', NULL, '2021-12-05', 'PAT', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
