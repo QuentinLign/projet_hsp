2ea671b6fbfb729dd10cb57b8a06da9e80c85f44
Quentin LIGNANI <48805997+QuentinLign@users.noreply.github.com>
2021-12-07 10:19:53 +0100

    JS Ajouter.php

--- adminkit-dev/ajouter.php
+++ adminkit-dev/ajouter.php
@@ -278,13 +278,8 @@ if(!isset($_SESSION['email']))
 
 </div>
 
-<script src="../js/app.js"></script>
-<script type="text/javascript" src="../js/jquery.min.js"></script>
-<script type="text/javascript" src="../js/popper.js"></script>
-<script type="text/javascript" src="../js/bootstrap.min.js"></script>
-<script type="text/javascript" src="../js/jquery.mCustomScrollbar.js"></script>
-<script type="text/javascript" src="../lib/slick/slick.min.js"></script>
-<script type="text/javascript" src="../js/script.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 
 </body>
 
